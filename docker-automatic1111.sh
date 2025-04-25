55e50c92093c2400064ca67c82542344dd6ff058
SonycProduction <104435023+SonycProduction@users.noreply.github.com>
2025-04-25 07:06:43 +0200

    Add files via upload

---
+++ docker-automatic1111.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prompt for sudo password at the start so script is not interrupted when docker commands execute
+sudo -v
+
+# Clone the repository
+git clone https://github.com/SonycProduction/docker-automatic1111.git
+
+# Navigate into the cloned directory
+cd docker-automatic1111
+
+# Run build and start for container
+sudo docker-compose up --build
